27032ee3e0d0b363502d15add1dd4216638a916c
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-05-14 00:55:35 +0200

    Format

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,10 +3,10 @@ import en from '@/locales/en.json'
 import { createI18n } from 'vue-i18n'
 
 const i18n = createI18n({
-	legacy: false, // you must set `false`, to use Composition API
-	locale: 'en',
-	fallbackLocale: 'en',
-	messages: { en, de }
+  legacy: false, // you must set `false`, to use Composition API
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: { en, de },
 })
 
 export default i18n
