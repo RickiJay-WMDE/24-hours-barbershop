633dec81db8516ccf3fd8db7760fe9b2cbc426e6
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-05-14 16:51:45 +0200

    Switcher

--- src/component/LangMenu.vue
+++ src/component/LangMenu.vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-const { locale, t, availableLocales } = useI18n()
+const { locale, availableLocales } = useI18n()
 
-const languages = computed(() =>
-  availableLocales.map((loc: string) => ({ title: t(`locale.lang_${loc}`), value: loc })),
-)
+const setLocale = (l: string) => (locale.value = l)
 </script>
 
 <template>
-  <div class="text-center lang-switcher">
-    <v-select
-      v-model="locale"
-      :items="languages"
-      density="compact"
-      hide-details
-      class="text-center lang-switcher header-text dim-text"
-    />
+  <div class="text-center lang-switcher header-text dim-text">
+    <template v-for="(l, idx) in availableLocales" :key="idx">
+      <template v-if="idx > 0">&vert;</template>
+      <v-btn density="compact" class="ma-0 pa-0 shrink" :onclick="() => setLocale(l)">
+        {{ l }}
+      </v-btn>
+    </template>
   </div>
 </template>
+
+<style lang="scss">
+.lang-switcher {
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 0;
+  font-size: 0.7rem;
+  font-weight: 400;
+  letter-spacing: 0.12em;
+  padding: 0;
+  .v-btn {
+    padding: 0;
+    width: 14px;
+  }
+}
+</style>
