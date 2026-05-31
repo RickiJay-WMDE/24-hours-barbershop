0ec641000af966c9c8ddb21e5d665958b643ba67
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-05-31 20:45:04 +0200

    Date Addition

--- source/src/component/TheSchedule.vue
+++ source/src/component/TheSchedule.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import ScheduleItem from '@/component/ScheduleItem.vue'
 import { eventStartDate } from '@/dates.ts'
+import { addHours, addMinutes } from 'date-fns'
 import { useI18n } from 'vue-i18n'
 
 const { t, tm } = useI18n()
@@ -22,98 +23,98 @@ const { t, tm } = useI18n()
           :copy="tm('schedule.events.gathering.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 11, 19, 30)"
+          :time="addMinutes(eventStartDate, 30)"
           affects="everyone"
           :title="t('schedule.events.drawing.title')"
           :copy="tm('schedule.events.drawing.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 11, 20)"
+          :time="addHours(eventStartDate, 1)"
           affects="arrangers"
           :title="t('schedule.events.begin.title')"
           :copy="tm('schedule.events.begin.copy')"
         />
         <schedule-item
           include-date
-          :time="new Date(2026, 8, 12)"
+          :time="addHours(eventStartDate, 5)"
           affects="singers"
           :title="t('schedule.events.tag_stop.title')"
           :copy="tm('schedule.events.tag_stop.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 1)"
+          :time="addHours(eventStartDate, 6)"
           affects="arrangers"
           :title="t('schedule.events.pancake_break.title')"
           :copy="tm('schedule.events.pancake_break.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 8)"
+          :time="addHours(eventStartDate, 13)"
           affects="everyone"
           :title="t('schedule.events.done.title')"
           :copy="tm('schedule.events.done.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 8)"
+          :time="addHours(eventStartDate, 13)"
           affects="singers"
           :title="t('schedule.events.rehearsal_start.title')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 8)"
+          :time="addHours(eventStartDate, 13)"
           affects="everyone"
           :title="t('schedule.events.prep.title')"
           :copy="tm('schedule.events.prep.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 9)"
+          :time="addHours(eventStartDate, 14)"
           affects="everyone"
           :title="t('schedule.events.sectionals.title')"
           :copy="tm('schedule.events.sectionals.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 10, 30)"
+          :time="addMinutes(addHours(eventStartDate, 15), 30)"
           affects="singers"
           :title="t('schedule.events.break.title')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 11)"
+          :time="addHours(eventStartDate, 16)"
           affects="singers"
           :title="t('schedule.events.ensemble.title')"
           :copy="tm('schedule.events.ensemble.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 12, 30)"
+          :time="addMinutes(addHours(eventStartDate, 17), 30)"
           affects="singers"
           :title="t('schedule.events.record.title')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 13)"
+          :time="addHours(eventStartDate, 18)"
           affects="singers"
           :title="t('schedule.events.lunch.title')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 13, 45)"
+          :time="addMinutes(addHours(eventStartDate, 18), 45)"
           affects="singers"
           :title="t('schedule.events.interpretation.title')"
           :copy="tm('schedule.events.interpretation.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 15)"
+          :time="addHours(eventStartDate, 20)"
           affects="everyone"
           :title="t('schedule.events.pippin.title')"
           :copy="tm('schedule.events.pippin.copy')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 17, 30)"
+          :time="addMinutes(addHours(eventStartDate, 22), 30)"
           affects="everyone"
           :title="t('schedule.events.break.title')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 18)"
+          :time="addHours(eventStartDate, 23)"
           affects="singers"
           :title="t('schedule.events.polish.title')"
         />
         <schedule-item
-          :time="new Date(2026, 8, 12, 20)"
+          :time="addHours(eventStartDate, 25)"
           affects="everyone"
           :title="t('schedule.events.concert.title')"
           :copy="tm('schedule.events.concert.copy')"
--- source/src/dates.ts
+++ source/src/dates.ts
@@ -1,4 +1,3 @@
 export const registrationDeadline = new Date(2026, 4, 24, 23, 59, 59)
 export const confirmationDate = new Date(2026, 5, 1)
 export const eventStartDate = new Date(2026, 8, 11, 19)
-export const eventEndDate = new Date(2026, 8, 12)
