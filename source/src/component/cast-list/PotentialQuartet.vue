92087638accbc2a026a6b7ed4bb447b38fc07d1d
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-06-06 03:53:48 +0200

    Split Voice

--- source/src/component/cast-list/PotentialQuartet.vue
+++ source/src/component/cast-list/PotentialQuartet.vue
@@ -1,153 +1,49 @@
 <script setup lang="ts">
-import PersonChip from '@/component/cast-list/PersonChip.vue'
-import useCastStore, { type Person } from '@/store/cast-store'
-import isValidPartialQuartet from '@/util/valid-partial-quartet'
-import { mdiClose, mdiThumbUpOutline } from '@mdi/js'
-import { computed, ref, watch } from 'vue'
-import { VueDraggableNext as draggable } from 'vue-draggable-next'
+import PotentialQuartetVoice from '@/component/cast-list/PotentialQuartetVoice.vue';
+import { type Person } from '@/store/cast-store';
+import isValidPartialQuartet from '@/util/valid-partial-quartet';
+import { ref } from 'vue';
 
 const { pushUnassigned } = defineProps<{ pushUnassigned: (v: string) => void }>()
 
-const store = useCastStore()
-const people = computed(() => store.people)
-
-const potentialTenorList = ref<string[]>([])
-watch(potentialTenorList, () => {
-  while (potentialTenorList.value.length > 1) {
-    const shifted = potentialTenorList.value.shift()
-    if (shifted) {
-      pushUnassigned(shifted)
-    }
-  }
-})
-const potentialTenor = computed((): Person | undefined =>
-  potentialTenorList.value[0] ? people.value[potentialTenorList.value[0]] : undefined,
-)
-
-const potentialLeadList = ref<string[]>([])
-watch(potentialLeadList, () => {
-  while (potentialLeadList.value.length > 1) {
-    const shifted = potentialLeadList.value.shift()
-    if (shifted) {
-      pushUnassigned(shifted)
-    }
-  }
-})
-const potentialLead = computed((): Person | undefined =>
-  potentialLeadList.value[0] ? people.value[potentialLeadList.value[0]] : undefined,
-)
-
-const potentialBariList = ref<string[]>([])
-watch(potentialBariList, () => {
-  while (potentialBariList.value.length > 1) {
-    const shifted = potentialBariList.value.shift()
-    if (shifted) {
-      pushUnassigned(shifted)
-    }
-  }
-})
-const potentialBari = computed((): Person | undefined =>
-  potentialBariList.value[0] ? people.value[potentialBariList.value[0]] : undefined,
-)
-
-const potentialBassList = ref<string[]>([])
-watch(potentialBassList, () => {
-  while (potentialBassList.value.length > 1) {
-    const shifted = potentialBassList.value.shift()
-    if (shifted) {
-      pushUnassigned(shifted)
-    }
-  }
-})
-const potentialBass = computed((): Person | undefined =>
-  potentialBassList.value[0] ? people.value[potentialBassList.value[0]] : undefined,
-)
+const potentialTenor = ref<Person | undefined>()
+const setPotentialTenor = (v: Person | undefined) => (potentialTenor.value = v)
+const potentialLead = ref<Person | undefined>()
+const setPotentialLead = (v: Person | undefined) => (potentialLead.value = v)
+const potentialBari = ref<Person | undefined>()
+const setPotentialBari = (v: Person | undefined) => (potentialBari.value = v)
+const potentialBass = ref<Person | undefined>()
+const setPotentialBass = (v: Person | undefined) => (potentialBass.value = v)
 </script>
 
 <template>
   <div class="list-column">
     <h3>Potential Quartet</h3>
     <div class="voices">
-      <div class="voice">
-        <div class="label">Tenor</div>
-        <div class="assign">
-          <draggable v-model="potentialTenorList" group="people" class="drag-area" :animation="150">
-            <div v-for="x in potentialTenorList" :key="x">
-              <person-chip v-if="people[x]" :person="people[x]" />
-            </div>
-          </draggable>
-        </div>
-        <div class="valid">
-          <v-icon
-            v-if="potentialTenor"
-            :icon="
-              isValidPartialQuartet(potentialTenor, undefined, undefined, undefined)
-                ? mdiThumbUpOutline
-                : mdiClose
-            "
-          />
-        </div>
-      </div>
-      <div class="voice">
-        <div class="label">Lead</div>
-        <div class="assign">
-          <draggable v-model="potentialLeadList" group="people" class="drag-area" :animation="150">
-            <div v-for="x in potentialLeadList" :key="x">
-              <person-chip v-if="people[x]" :person="people[x]" />
-            </div>
-          </draggable>
-        </div>
-        <div class="valid">
-          <v-icon
-            v-if="potentialLead"
-            :icon="
-              isValidPartialQuartet(undefined, potentialLead, undefined, undefined)
-                ? mdiThumbUpOutline
-                : mdiClose
-            "
-          />
-        </div>
-      </div>
-      <div class="voice">
-        <div class="label">Bari</div>
-        <div class="assign">
-          <draggable v-model="potentialBariList" group="people" class="drag-area" :animation="150">
-            <div v-for="x in potentialBariList" :key="x">
-              <person-chip v-if="people[x]" :person="people[x]" />
-            </div>
-          </draggable>
-        </div>
-        <div class="valid">
-          <v-icon
-            v-if="potentialBari"
-            :icon="
-              isValidPartialQuartet(undefined, undefined, potentialBari, undefined)
-                ? mdiThumbUpOutline
-                : mdiClose
-            "
-          />
-        </div>
-      </div>
-      <div class="voice">
-        <div class="label">Bass</div>
-        <div class="assign">
-          <draggable v-model="potentialBassList" group="people" class="drag-area" :animation="150">
-            <div v-for="x in potentialBassList" :key="x">
-              <person-chip v-if="people[x]" :person="people[x]" />
-            </div>
-          </draggable>
-        </div>
-        <div class="valid">
-          <v-icon
-            v-if="potentialBass"
-            :icon="
-              isValidPartialQuartet(undefined, undefined, undefined, potentialBass)
-                ? mdiThumbUpOutline
-                : mdiClose
-            "
-          />
-        </div>
-      </div>
+      <potential-quartet-voice
+        label="Tenor"
+        voice="tenor"
+        :push-unassigned="pushUnassigned"
+        :set-potential-voice="setPotentialTenor"
+      />
+      <potential-quartet-voice
+        label="Lead"
+        voice="lead"
+        :push-unassigned="pushUnassigned"
+        :set-potential-voice="setPotentialLead"
+      />
+      <potential-quartet-voice
+        label="Bari"
+        voice="bari"
+        :push-unassigned="pushUnassigned"
+        :set-potential-voice="setPotentialBari"
+      />
+      <potential-quartet-voice
+        label="Bass"
+        voice="bass"
+        :push-unassigned="pushUnassigned"
+        :set-potential-voice="setPotentialBass"
+      />
     </div>
     <div class="valid">
       {{ isValidPartialQuartet(potentialTenor, potentialLead, potentialBari, potentialBass) }}
@@ -156,17 +52,4 @@ const potentialBass = computed((): Person | undefined =>
 </template>
 
 <style lang="scss">
-.voice {
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-between;
-  gap: 6px;
-  margin: 6px 0;
-  .assign {
-    flex-grow: 1;
-    .drag-area {
-      min-height: 32px;
-    }
-  }
-}
 </style>
---
+++ source/src/component/cast-list/PotentialQuartetVoice.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import PersonChip from '@/component/cast-list/PersonChip.vue'
+import useCastStore, { type Person, type RangeType } from '@/store/cast-store'
+import isValidPartialQuartet from '@/util/valid-partial-quartet'
+import { mdiClose, mdiThumbUpOutline } from '@mdi/js'
+import { computed, ref, watch } from 'vue'
+import { VueDraggableNext as draggable } from 'vue-draggable-next'
+
+const { pushUnassigned, setPotentialVoice, voice } = defineProps<{
+  label: string
+  pushUnassigned: (v: string) => void
+  setPotentialVoice: (v: Person | undefined) => void
+  voice: RangeType
+}>()
+
+const store = useCastStore()
+const people = computed(() => store.people)
+
+const potentialVoiceList = ref<string[]>([])
+watch(potentialVoiceList, () => {
+  while (potentialVoiceList.value.length > 1) {
+    const shifted = potentialVoiceList.value.shift()
+    if (shifted) {
+      pushUnassigned(shifted)
+    }
+  }
+})
+const potentialVoice = computed((): Person | undefined =>
+  potentialVoiceList.value[0] ? people.value[potentialVoiceList.value[0]] : undefined,
+)
+watch(potentialVoice, () => setPotentialVoice(potentialVoice.value))
+
+const valid = computed(() =>
+  voice == 'tenor'
+    ? isValidPartialQuartet(potentialVoice.value, undefined, undefined, undefined)
+    : voice == 'lead'
+      ? isValidPartialQuartet(undefined, potentialVoice.value, undefined, undefined)
+      : voice == 'bari'
+        ? isValidPartialQuartet(undefined, undefined, potentialVoice.value, undefined)
+        : voice == 'bass'
+          ? isValidPartialQuartet(undefined, undefined, undefined, potentialVoice.value)
+          : false,
+)
+</script>
+
+<template>
+  <div class="voice">
+    <div class="label">{{ label }}</div>
+    <div class="assign">
+      <draggable v-model="potentialVoiceList" group="people" class="drag-area" :animation="150">
+        <div v-for="x in potentialVoiceList" :key="x">
+          <person-chip v-if="people[x]" :person="people[x]" :range="voice" />
+        </div>
+      </draggable>
+    </div>
+    <div class="valid">
+      <v-icon v-if="potentialVoice" :icon="valid ? mdiThumbUpOutline : mdiClose" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.voice {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  gap: 6px;
+  margin: 6px 0;
+  .assign {
+    flex-grow: 1;
+    .drag-area {
+      min-height: 32px;
+    }
+  }
+}
+</style>
